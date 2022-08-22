1268b4e58778e054b150d638ff2c8d956a1edbeb
aswinraj <aswinrajkailath@gmail.com>
2022-08-23 00:58:25 +0530

    Updated the UI with tailwind

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract, utils } from "ethers";
 import { useEffect, useRef, useState } from "react";
@@ -164,41 +164,62 @@ export default function Home() {
       if (inputMode) {
         return (
           <>
-            <div className={styles.description}>
+            <div className="text-2xl text-slate-500 drop-shadow-lg mb-4 text-center">
               Choose any Mode: Either issue a certificate or verify one.
             </div>
-            <button type="submit" onClick={issueMode}>
-              Issue
-            </button>
-            <button type="submit" onClick={verifyMode}>
-              Verify
-            </button>
-            <div>Issue Mode</div>
+            <div className="flex justify-center py-4 border-b border-slate-500">
+              <button
+                type="submit"
+                onClick={issueMode}
+                className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg"
+              >
+                <a>Issue</a>
+              </button>
+              <a>
+                <button
+                  type="submit"
+                  onClick={verifyMode}
+                  className="ml-4 inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600  rounded text-lg"
+                >
+                  Verify
+                </button>
+              </a>
+            </div>
+            <div className="text-3xl text-slate-800 font-medium mt-6 mb-4">
+              Issue Mode
+            </div>
             <form
+              className="flex flex-col"
               onSubmit={(event) => {
                 event.preventDefault();
                 addDoc();
               }}
             >
               <input
+                className="border-2 hover:border-blue-600 my-2 "
                 type="text"
                 placeholder="Enter CID!"
                 value={cidValue}
                 onChange={onCidChange}
               />
               <input
+                className="border-2 hover:border-blue-600 my-2 "
                 type="text"
                 placeholder="Enter doc link!"
                 value={inputValue}
                 onChange={onInputChange}
               />
               <input
+                className="border-2 hover:border-blue-600 my-2 "
                 type="text"
                 placeholder="Enter receiver address!"
                 value={recAddress}
                 onChange={onRecAddressChange}
               />
-              <button type="submit" className={styles.button}>
+              <button
+                type="submit"
+                className="mt-2 text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600  rounded text-lg"
+              >
                 Issue Certificate
               </button>
             </form>
@@ -206,75 +227,135 @@ export default function Home() {
         );
       } else if (!inputMode) {
         return (
-          <>
-            <div className={styles.description}>
+          <div>
+            <div className="text-2xl text-slate-500 drop-shadow-lg  text-center">
               Choose any Mode: Either issue a certificate or verify one.
             </div>
-            <button type="submit" onClick={issueMode}>
+            {/* <button type="submit" onClick={issueMode} className="test-2xl">
               Issue
             </button>
             <button type="submit" onClick={verifyMode}>
               Verify
-            </button>
-            <div> verification Mode</div>
+            </button> */}
+            <div className="flex justify-center my-4 py-4 border-b border-slate-500">
+              <button
+                type="submit"
+                onClick={issueMode}
+                className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg"
+              >
+                <a>Issue</a>
+              </button>
+              <a>
+                <button
+                  type="submit"
+                  onClick={verifyMode}
+                  className="ml-4 inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600  rounded text-lg"
+                >
+                  Verify
+                </button>
+              </a>
+            </div>
+            <div className="text-3xl text-slate-800 font-medium mt-6 mb-4">
+              {" "}
+              Verification Mode
+            </div>
             <form
+              className="flex flex-col"
               onSubmit={(event) => {
                 event.preventDefault();
                 verifyDoc();
               }}
             >
               <input
+                className="border-2 hover:border-blue-600 my-2 "
                 type="text"
                 placeholder="Enter CID!"
                 value={cidValue}
                 onChange={onCidChange}
               />
 
-              <button type="submit" className={styles.button}>
+              <button
+                type="submit"
+                className="mt-2 mb-8 text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600  rounded text-lg"
+              >
                 Verify Certificate
               </button>
             </form>
-            <div>
+            <div className="w-full px-4 py-6 rounded shadow-lg">
               {verifiedDoc && (
-                <div>
-                  <img src={verifiedDoc[1]} />
-                  <p>CID:</p>
-                  <span>{verifiedDoc[0]}</span>
-                  <p>Doclink:</p>
-                  <span>{verifiedDoc[1]}</span>
-                  <p>Issuer:</p>
-                  <span>{verifiedDoc[2]}</span>
-                  <p>Receiver:</p>
-                  <span>{verifiedDoc[3]}</span>
+                <div className="">
+                  <img className="max-h[300px]" src={verifiedDoc[1]} />
+                  <div className="mt-4 ">
+                    <p className="text-xl font-semibold text-purple-800 ">
+                      CID:
+                    </p>
+                    <span>
+                      <p className="text-lg mb-2 text-slate-800">
+                        {verifiedDoc[0]}
+                      </p>
+                    </span>
+                    <p className="text-xl font-semibold text-purple-800">
+                      Doclink:
+                    </p>
+                    <span>
+                      <p className="text-lg mb-2 text-slate-800">
+                        {verifiedDoc[1]}
+                      </p>
+                    </span>
+                    <p className="text-xl font-semibold text-purple-800">
+                      Issuer:
+                    </p>
+                    <span>
+                      <p className="text-lg mb-2 text-slate-800">
+                        {verifiedDoc[2]}
+                      </p>
+                    </span>
+                    <p className="text-xl font-semibold text-purple-800">
+                      Receiver:
+                    </p>
+                    <span>
+                      <p className="text-lg mb-2 text-slate-800">
+                        {verifiedDoc[3]}
+                      </p>
+                    </span>
+                  </div>
                 </div>
               )}
             </div>
-          </>
+          </div>
         );
       } else if (loading) {
-        return <button className={styles.button}>Loading...</button>;
+        return <button className="">Loading...</button>;
       } else {
         return (
           <>
-            <div className={styles.description}>
+            <div className="text-2xl text-slate-500 drop-shadow-lg border border-slate-800 text-center">
               Choose any Mode: Either issue a certificate or verify one.
             </div>
-            <button type="submit" onClick={issueMode}>
-              Issue
-            </button>
-            <button
-              type="submit"
-              className="cta-button cta-position submit-gif-button"
-              onClick={verifyMode}
-            >
-              Verify
-            </button>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                onClick={issueMode}
+                className="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg"
+              >
+                <a>Issue</a>
+              </button>
+              <a>
+                <button
+                  type="submit"
+                  onClick={verifyMode}
+                  className="ml-4 inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600  rounded text-lg"
+                >
+                  Verify
+                </button>
+              </a>
+            </div>
           </>
         );
       }
     } else {
       return (
-        <button onClick={connectWallet} className={styles.button}>
+        <button onClick={connectWallet} className="">
           Connect your wallet
         </button>
       );
@@ -308,21 +389,55 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.main}>
-        <div>
-          <h1 className={styles.title}>PinDown</h1>
-          <div className={styles.description}>Easy Certificate Verifier</div>
-          {/* <div className={styles.description}>
-            {numberOfWhitelisted} have already joined the Whitelist
-          </div> */}
-          {renderButton()}
+      <div className=" bg-gradient-to-r  w-full from-rose-50 to-teal-50">
+        <header className="text-gray-800 font-semibold md:sticky top-0 z-10 body-font backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 shadow-lg">
+          <div className="container mx-auto flex px-5 py-4 flex-row items-center justify-between">
+            <a className="flex title-font font-medium items-center text-gray-700  md:mb-0">
+              {/* <img src="img/pinhead.png" alt="Pindown" width="60" /> */}
+              <span className="ml-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-violet-700 via-purple-800 to-violet-600">
+                <h2>PinDown.</h2>
+              </span>
+            </a>
+          </div>
+        </header>
+        <section className="text-gray-700 body-font ">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center md:max-w-[1240px]">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-5xl text-4xl mb-4 font-medium text-gray-500">
+                <a className="text-gray-800 font-semibold">PinDown </a>
+                <br className="hidden lg:inline-block" />
+                Certificate Issuer and verifier
+              </h1>
+              <p className="mb-8 leading-relaxed text-lg md:text-xl">
+                Pindown is a blockchain-based document verifier. Verify the
+                authenticity of any certificate.
+              </p>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center rounded max-h-64"
+                alt="hero"
+                src="https://cdn.pixabay.com/photo/2013/07/12/17/00/drawing-pin-151658__340.png"
+              />
+            </div>
+          </div>
+        </section>
+        <div className="flex items-center w-full mb-16">
+          <div className="max-w-[1200px] mx-auto">{renderButton()}</div>
         </div>
-        <div>
-          {/* <img className={styles.image} src="./crypto-devs.svg" /> */}
-        </div>
-      </div>
 
-      <footer className={styles.footer}>Made with &#10084; by Trizwit</footer>
+        <footer className="text-gray-500 bg-gradient-to-r  w-full from-rose-50 to-teal-50 border-t-2 border-gray-300 shadow-xl body-font">
+          <div className="container px-5 py-4 mx-auto flex items-center sm:flex-row flex-col">
+            <a className="flex title-font font-medium items-center md:justify-start justify-center text-violet-500">
+              {/* <img src="img/zindothead.png" alt="Zindot" width="50" height="50" /> */}
+              <span className="ml-3 text-2xl">PinDown.</span>
+            </a>
+            <p className="text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-300 sm:py-2 sm:mt-0 mt-4">
+              Made with &#10084; by Trizwit
+            </p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
